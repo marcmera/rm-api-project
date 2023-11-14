c60cd3505c61927286e0191d6e8b6fd7038699a0
marcmera <marcmersua@campus.monlau.com>
2023-11-14 18:24:49 +0100

    Refactor getCharacters function to return only results array

--- new/frontend/src/api/rm.api.js
+++ new/frontend/src/api/rm.api.js
@@ -3,7 +3,8 @@ export async function getCharacters() {
     const response = await fetch("https://rickandmortyapi.com/api/character/");
     const json = await response.json();
     const data = JSON.stringify(json);
-    return data;
+    const formated = JSON.parse(data);
+    return formated.results;
     // updateCharacterCardColors(data);
   } catch (error) {
     console.error("Error fetching characters:", error);
