25ec7aec2a64bb884c8125763905282aa98f1bd0
marcmera <marcmersua@campus.monlau.com>
2023-11-15 19:05:51 +0100

    Update fonts and home page image

--- new/frontend/src/pages/Home.jsx
+++ new/frontend/src/pages/Home.jsx
@@ -1,16 +1,13 @@
+import img from "../assets/home-img.png";
+
 export function Home() {
   return (
     <div className="home-page">
       <img
-        className="w-full h-full position-absolute opacity-80"
-        src="https://c4.wallpaperflare.com/wallpaper/365/629/82/rick-morty-rick-and-morty-cartoons-wallpaper-preview.jpg"
+        className="w-full h-full position-absolute opacity-90"
+        src={img}
         alt="home-img"
       />
-      <h1 className="text-8xl pt-40 absolute">
-        <span className="text-cyan-500">Rick</span>
-        <span className="text-green-300"> and </span>
-        <span className="text-yellow-300">Morty</span>
-      </h1>
     </div>
   );
 }
