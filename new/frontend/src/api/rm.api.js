24f864f776c90488668c588660afe3775644f6fd
marcmera <marcmersua@campus.monlau.com>
2023-11-15 15:24:07 +0100

    fourth-commit

--- new/frontend/src/api/rm.api.js
+++ new/frontend/src/api/rm.api.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 export async function getCharacters(id) {
   try {
     const response = await fetch(
@@ -8,14 +7,6 @@ export async function getCharacters(id) {
     const data = JSON.stringify(json);
     const formated = JSON.parse(data);
     return formated.results;
-=======
-export async function getCharacters() {
-  try {
-    const response = await fetch("https://rickandmortyapi.com/api/character/");
-    const json = await response.json();
-    const data = JSON.stringify(json);
-    return data;
->>>>>>> 29ea3dc466a8d95d7fa5722f719f0d42f5bdd6ec
     // updateCharacterCardColors(data);
   } catch (error) {
     console.error("Error fetching characters:", error);
