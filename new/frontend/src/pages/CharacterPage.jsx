9793d1791851faa4b4ba28804663e11739e1e98a
MarcMera <marcmera19@gmail.com>
2023-11-22 16:22:59 +0100

    Update navigation and remove footer from pages

--- new/frontend/src/pages/CharacterPage.jsx
+++ new/frontend/src/pages/CharacterPage.jsx
@@ -1,13 +1,11 @@
 import { Header } from "../components/Header";
 import { CharactersList } from "../components/CharactersList";
-import { Footer } from "../components/Footer";
 
 export function CharacterPage() {
   return (
     <div>
       <Header title="Characters" />
       <CharactersList />
-      <Footer />
     </div>
   );
 }
--- new/frontend/src/pages/Episodes.jsx
+++ new/frontend/src/pages/Episodes.jsx
@@ -1,12 +1,10 @@
 import { Header } from "../components/Header";
-import { Footer } from "../components/Footer";
 
 export function Episodes() {
     return (
         <div>
         <Header title="Episodes" />
         {/* <EpisodesList /> */}
-        <Footer />
         </div>
     );
     }
